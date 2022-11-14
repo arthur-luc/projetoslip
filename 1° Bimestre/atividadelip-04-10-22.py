92686537c81976e0f881548a7f31e441699eac36
Arthur Lucas Rodrigues <lucas.arthur@aluno.ifsp.edu.br>
2022-11-14 01:31:28 -0300

    Create atividadelip-04-10-22.py

---
+++ 1° Bimestre/atividadelip-04-10-22.py
@@ -0,0 +1,16 @@
+#calculadora básica
+print("==================Começo processamento=====================")
+
+#solicitando números
+num1 = input("Insira o primeiro número\n")
+num2 = input("Insira o segundo número\n")
+
+#realizando as operacoes
+print('A soma dos dois números é igual a:' , int(num1) + int(num2))
+
+print('A subtração dos dois números é igual a:' , int(num1) - int(num2))
+
+print('A divisão dos dois números é igual a:' , int(num1) / int(num2))
+
+print('O módulo dos dois números é igual a:' , int(num1) % int(num2))
+print("===================Fim processamento=======================")
